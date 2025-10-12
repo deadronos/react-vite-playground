92eca50a0824210e62ecfdfe780881d29d51180f
deadronos <iridanblub@hotmail.de>
2025-10-12 16:55:47 +0200

    feat: add r3f-stage dependency and implement R3FStage component
    - Added "r3f-stage" package to package.json
    - Removed unused playground.tsx component
    - Created new r3fstage.tsx component utilizing r3f-stage for rendering a basic scene

--- src/components/playground.tsx
+++
@@ -1,2 +0,0 @@
-
-import * as RC from "render-composer"
---
+++ src/components/r3fstage.tsx
@@ -0,0 +1,17 @@
+import { Application } from "r3f-stage"
+
+/* r3f-stage provides a global stylesheet. Please import it in your application and remove any other global styles you may have defined. */
+import "r3f-stage/styles.css"
+
+function R3FStage() {
+  return (
+    <Application>
+      <mesh>
+        <dodecahedronGeometry />
+        <meshStandardMaterial />
+      </mesh>
+    </Application>
+  )
+}
+
+export default R3FStage;
