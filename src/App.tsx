7668ef4823bf097ea84db2ea322f273fe0f83861
deadronos <iridanblub@hotmail.de>
2025-10-13 04:20:17 +0200

    feat: implement responsive layout with flexbox and enhance 3D view in R3FView component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import type { ReactElement } from 'react'
 import * as React from 'react';
 import './App.css'
-import { Theme, ThemePanel, Box, Card, Flex, Grid, Container } from "@radix-ui/themes"
+import { Theme, ThemePanel, Text, Box, Card, Flex, Grid, Container } from "@radix-ui/themes"
+import R3FView from './components/r3fview';
+import { GlobalProvider } from '@triplex/provider';
+import { asChildPropDef } from '@radix-ui/themes/props';
+
 
 
 
@@ -11,12 +15,37 @@ function App(): ReactElement {
 
 
   return (
+    <GlobalProvider>
     <Theme appearance={'dark'} hasBackground={true} accentColor={'indigo'}>
-      <Container width="100%" maxWidth="100%">
-        <Flex>
-          <Grid columns="3" gap="3" width="100%">
+      <Box className={"windowcontainer"} width="100%" height="100%">
+        <Box asChild className={"headercontainer"}>
+          <Flex>
+            <Grid align="center" columns="3" gap="3" width="100%" style={{placeItems:'center'}}>
+              <Card>
+                <Box gridColumn="1">
+                  1
+                </Box>
+              </Card>
+              <Card>
+                <Box gridColumn="2">
+                  Deadronos Collection
+                </Box>
+              </Card>
+              <Card>
+                <Box gridColumn="3">
+                  3
+                </Box>
+              </Card>
+            </Grid>
+          </Flex>
+        </Box>
+        <Box asChild className="r3fview">
+          <R3FView />
+        </Box>
+        <Box asChild className="footercontainer">
+          <Grid align="center" columns="3" gap="3" width="100%" style={{placeItems:'center'}}>
             <Card>
-              <Box gridColumn="1" >
+              <Box gridColumn="1">
                 1
               </Box>
             </Card>
@@ -31,10 +60,11 @@ function App(): ReactElement {
               </Box>
             </Card>
           </Grid>
-        </Flex>
-      </Container>
-      <ThemePanel />
+        </Box>
+      </Box>
+      <ThemePanel defaultOpen={false}/>
     </Theme>
+    </GlobalProvider>
   )
 }
 
--- src/components/r3fview.tsx
+++ src/components/r3fview.tsx
@@ -1,18 +1,35 @@
-import * as RC from 'render-composer'
+import * as THREE from 'three'
+import { type ReactElement } from 'react'
+import { CanvasProvider } from '@triplex/provider';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
+import { PerspectiveCamera } from "@react-three/drei";
 
 
-function R3FView() {
+
+
+function R3FView(): ReactElement {
   return (
-    <RC.Canvas strict>
-      <RC.RenderPipeline>
-        <mesh>
-        <dodecahedronGeometry />
-        <meshStandardMaterial />
-      </mesh>
-      </RC.RenderPipeline>
-    </RC.Canvas>
-
-  );
+    <CanvasProvider>
+      <Canvas className='r3fview'>
+        <>
+          <pointLight name={"Key Light"} position={[1.33, 0.64, 1.99]} intensity={5} color={"#f0f0ff"} />
+          <pointLight name={"Fill Light"} position={[-2.47, 2.49, -0.16]} rotation={[-1.122659331488609, -0.948099223993744, 1.8730258687794386]} castShadow={false} intensity={2.5} />
+          <pointLight name={"Back Light"} position={[-0.09, 1.07, -3.98]} rotation={[-1.122659331488609, -0.948099223993744, 1.8730258687794386]} castShadow={false} intensity={2.5} />
+          <PerspectiveCamera position={[0.62, 2, 4.02]} makeDefault={true} />
+          <OrbitControls />
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial color={"#ff2424"} />
+          </mesh>
+          <mesh position={[0, 1.56, 0]}>
+            <sphereGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        </>
+      </Canvas>
+    </CanvasProvider>
+  )
 }
 
 export default R3FView;
