a51a42ed429099c67fc8dc3c4058010538e15e82
deadronos <iridanblub@hotmail.de>
2025-10-17 01:11:58 +0200

    feat: update R3FView camera, orbit controls and transmission mesh
    Set PerspectiveCamera to a new position/rotation for a better initial view.
    Enable OrbitControls autoRotate and lock zoom/rotate/pan for a controlled animated presentation.
    Increase transmission sphere scale from [2,2,2] to [3,3,3].
    Clean up MeshTransmissionMaterial props by removing unused/verbose blend/stencil fields.

--- src/components/r3fview.tsx
+++ src/components/r3fview.tsx
@@ -17,8 +17,8 @@ function R3FView(): ReactElement {
           <directionalLight name={"Key Light"} position={[1.33, 1.51, 5.59]} intensity={3} color={"#f0f0ff"} castShadow={true} />
           <pointLight name={"Fill Light"} position={[-2.47, 2.49, -0.16]} rotation={[-1.122659331488609, -0.948099223993744, 1.8730258687794386]} color={"#f0f0ff"} castShadow={true} intensity={2.5} />
           <pointLight name={"Back Light"} position={[-0.09, 1.07, -3.98]} rotation={[-1.122659331488609, -0.948099223993744, 1.8730258687794386]} color={"#f0f0ff"} castShadow={false} intensity={2.5} />
-          <PerspectiveCamera position={[0.62, 2, 4.02]} makeDefault={true} />
-          <OrbitControls />
+          <PerspectiveCamera position={[4.62, 7, -6.01]} makeDefault={true} rotation={[-2.9434326494568124, 0.5148495139047417, 3.041670718291418]} />
+          <OrbitControls autoRotate={true} enableZoom={false} enableRotate={false} enablePan={false} />
           <mesh>
             <boxGeometry />
             <meshStandardMaterial color={"#5257ff"} />
@@ -27,9 +27,9 @@ function R3FView(): ReactElement {
             <sphereGeometry />
             <meshStandardMaterial color={"indigo"} />
           </mesh>
-          <mesh position={[0, 1.18, 0]} scale={[2, 2, 2]}>
-            <sphereGeometry />
-            <MeshTransmissionMaterial transmission={1.0} clearcoat={1} color={"#c9ffa1"} vertexColors={false} reflectivity={0.44} opacity={undefined} alphaToCoverage={false} colorWrite={true} clipShadows={false} blendSrc={undefined} blendColor={"#000000"} stencilFail={undefined} blendSrcAlpha={undefined} stencilWrite={false} anisotropicBlur={0.1} attenuationColor={"#000000"} backside={true} chromaticAberration={undefined} forceSinglePass={false} iridescence={undefined} isMeshPhysicalMaterial={false} precision={"mediump"} specularColor={"#000000"} sheenColor={"#000000"} wireframeLinecap={undefined} thickness={0.2} depthFunc={3} clipIntersection={false} bumpScale={undefined} displacementBias={undefined} displacementScale={undefined} fog={false} flatShading={false} emissiveIntensity={0.05} emissive={"indigo"} dithering={false} alphaTest={undefined} alphaHash={false} />
+          <mesh position={[0, 1.18, 0]} scale={[3, 3, 3]}>
+          <sphereGeometry />
+          <MeshTransmissionMaterial transmission={1.0} clearcoat={1} color={"#c9ffa1"} vertexColors={false} reflectivity={0.44} opacity={undefined} alphaToCoverage={false} colorWrite={true} clipShadows={false} anisotropicBlur={0.1} attenuationColor={"#000000"} backside={true} chromaticAberration={undefined} forceSinglePass={false} iridescence={undefined} isMeshPhysicalMaterial={false} precision={"mediump"} specularColor={"#000000"} sheenColor={"#000000"} wireframeLinecap={undefined} thickness={0.2} depthFunc={3} clipIntersection={false} bumpScale={undefined} displacementBias={undefined} displacementScale={undefined} fog={false} flatShading={false} emissiveIntensity={0.05} emissive={"indigo"} dithering={false} alphaTest={undefined} alphaHash={false} />
           </mesh>
         </>
       </Canvas>
